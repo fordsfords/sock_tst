80093ea5630074f6d84c5cb0f326b2fc5a326f4b
Steve Ford <sford@geeky-boy.com>
2024-09-06 09:42:51 -0400

    updates

--- win_close.c
+++ win_close.c
@@ -11,19 +11,21 @@
 #define DEFAULT_PORT "12000"
 
 int main(int argc, char **argv) {
-    WSADATA wsa_data;
-    SOCKET listen_sock = INVALID_SOCKET;
-    SOCKET accepted_sock;
-    int status;
-    struct addrinfo *addr_info = NULL, hints;
+  WSADATA wsa_data;
+  SOCKET listen_sock = INVALID_SOCKET;
+  SOCKET accepted_sock;
+  int status;
+  struct addrinfo *addr_info = NULL, hints;
+  LINGER linger = {1, 0};
 
-    /* Initialize Winsock */
-    status = WSAStartup(MAKEWORD(2,2), &wsa_data);
-    if (status != 0) {
-        printf("WSAStartup failed: %d\n", status);
-        exit(1);
-    }
+  /* Initialize Winsock */
+  status = WSAStartup(MAKEWORD(2,2), &wsa_data);
+  if (status != 0) {
+    printf("WSAStartup failed: %d\n", status);
+    exit(1);
+  }
 
+  while (1) {
     ZeroMemory(&hints, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
@@ -33,52 +35,50 @@ int main(int argc, char **argv) {
     /* Resolve the local address and port to be used by the server */
     status = getaddrinfo(NULL, DEFAULT_PORT, &hints, &addr_info);
     if (status != 0) {
-        printf("getaddrinfo failed: %d\n", status);
-        exit(1);
+      printf("getaddrinfo failed: %d\n", status);
+      exit(1);
     }
     
     /* Create a SOCKET for the server to listen for client connections */
     listen_sock = socket(addr_info->ai_family, addr_info->ai_socktype, addr_info->ai_protocol);
     if (listen_sock == INVALID_SOCKET) {
-        printf("Error at socket(): %ld\n", WSAGetLastError());
-        exit(1);
+      printf("Error at socket(): %ld\n", WSAGetLastError());
+      exit(1);
     }
 
     /* Setup the TCP listening socket */
     status = bind(listen_sock, addr_info->ai_addr, (int)addr_info->ai_addrlen);
     if (status == SOCKET_ERROR) {
-        printf("bind failed with error: %d\n", WSAGetLastError());
-        exit(1);
+      printf("bind failed with error: %d\n", WSAGetLastError());
+      exit(1);
     }
 
     freeaddrinfo(addr_info);
 
     /* Start listening for connections */
     if (listen(listen_sock, SOMAXCONN) == SOCKET_ERROR) {
-        printf("Listen failed with error: %ld\n", WSAGetLastError());
-        closesocket(listen_sock);
-        exit(1);
+      printf("Listen failed with error: %ld\n", WSAGetLastError());
+      exit(1);
     }
 
     printf("Waiting for a connection...\n");
 
     accepted_sock = accept(listen_sock, NULL, NULL);
     if (accepted_sock == INVALID_SOCKET) {
-        printf("accept failed: %d\n", WSAGetLastError());
-        closesocket(listen_sock);
-        exit(1);
+      printf("accept failed: %d\n", WSAGetLastError());
+      exit(1);
     }
 
     /* Forcefully close the socket */
-    LINGER linger = {1, 0};
     setsockopt(accepted_sock, SOL_SOCKET, SO_LINGER, (char *)&linger, sizeof(linger));
     closesocket(accepted_sock);
 
     /* Cleanup */
     closesocket(listen_sock);
-    WSACleanup();
 
-    printf("Connection closed. Exiting.\n");
+    printf("Connection closed\n");
+  }  /* while 1 */
 
-    return 0;
-}
+  WSACleanup();
+  return 0;
+}  /* main */
